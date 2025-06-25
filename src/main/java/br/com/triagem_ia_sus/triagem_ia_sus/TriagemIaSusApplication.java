f3b6d2c3246ddd975a94ec630af240eb7c4045bc
Joao Aguiar Descktop <joaovitoraguiarsilva@gmail.com>
2025-06-24 21:05:45 -0300

    Primeiro commit

---
+++ src/main/java/br/com/triagem_ia_sus/triagem_ia_sus/TriagemIaSusApplication.java
@@ -0,0 +1,13 @@
+package br.com.triagem_ia_sus.triagem_ia_sus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriagemIaSusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriagemIaSusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/triagem_ia_sus/triagem_ia_sus/TriagemIaSusApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.triagem_ia_sus.triagem_ia_sus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriagemIaSusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
